2c68f3738ecae52cc8faf75fdb88e5a195a1006e
wajaht-ali <contact.chwajahat@gmail.com>
2025-02-07 13:50:20 +0500

    Database credentials updated

--- services/db.js
+++ services/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+const dbConnStr = "localhost:5127";
 export const connectDB = async () => {
   try {
     mongoose.connection.on("connected", () => {
@@ -11,7 +12,7 @@ export const connectDB = async () => {
     });
 
     await mongoose.connect(
-      "mongodb+srv://wajahat:cY2eCJ3mleOeLcrU@cluster0.jnar07p.mongodb.net/mimar-db"
+      `${dbConnStr}/mimar-db`
     );
   } catch (error) {
     console.log("❌Unable to connect with DB.", error);
